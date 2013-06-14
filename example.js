5720fe4db51fc779afcf296f14cab06fe26c8197
Maximilian Krüger <kruemaxi@googlemail.com>
2013-06-14 19:02:35 +0200

    simplify example

--- example.js
+++ example.js
@@ -4,19 +4,14 @@ var celerity = require('./src/celerity');
 
 var redisClient = redis.createClient();
 
-var oneMinuteMs = 60 * 1000
-
 var config = {
     redis: redisClient,
-    timespanMs: oneMinuteMs,
+    timespanMs: 10 * 1000,
     bucketCount: 10
 };
 
-celerity.increment(config, 'test', 1, function(err) {
+celerity.incrementAndRead(config, 'test', 1, function(err, rate) {
     if (err) throw err;
-    celerity.read(config, 'test', function(err, count) {
-        if (err) throw err;
-        console.log(count);
-        redisClient.quit()
-    });
+    console.log(rate);
+    redisClient.quit()
 });
