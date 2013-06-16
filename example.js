08464061dd61a6eb958d5f1ead835946070295b9
Maximilian Krüger <kruemaxi@googlemail.com>
2013-06-16 22:27:07 +0200

    make example a simple rate limited webserver

--- example.js
+++ example.js
@@ -1,17 +1,37 @@
-var redis = require('redis');
+var http = require('http');
 
-var celerity = require('./src/celerity');
+var redis = require('redis');
+var celerity = require('celerity');
 
 var redisClient = redis.createClient();
 
-var config = {
+var celerityConfig = {
     redis: redisClient,
     timespanMs: 10 * 1000,
     bucketCount: 10
 };
 
-celerity.incrementAndRead(config, 'test', 1, function(err, rate) {
-    if (err) throw err;
-    console.log(rate);
-    redisClient.quit()
+var server = http.createServer(function(req, res) {
+    // bounce favicon
+    if (req.url == '/favicon.ico') {
+        res.statusCode = 404;
+        res.end();
+        return
+    }
+
+    celerity.read(celerityConfig, 'requests', 1, function(err, rate) {
+        if (err) {
+            return res.end('there was an error');
+        }
+
+        if (rate > 10) {
+            return res.end('whoa. you are going way too fast');
+        }
+
+        res.end(rate + ' requests in the last 10 seconds');
+    });
 });
+
+server.listen(8080);
+
+console.log('go to port 8080');
